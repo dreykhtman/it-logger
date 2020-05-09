0ec751196740ddd18bb4b006c25d1e9112fdc49c
Dima <dreykhtman@gmail.com>
2020-05-08 20:00:13 -0400

    finished section 11.77

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 /* eslint-disable import/extensions */
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
     M.AutoInit();
   });
-  return <div className="App">Hello World</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
