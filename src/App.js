3b88e8025092b521e3ff461f1a0bb5e811a4db5b
Dima <dreykhtman@gmail.com>
2020-05-09 14:09:05 -0400

    finished section 11.79

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = () => {
   useEffect(() => {
-    M.AutoInit();
+    M.AutoInit(); // initialize Materialize JS
   });
+
   return (
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
